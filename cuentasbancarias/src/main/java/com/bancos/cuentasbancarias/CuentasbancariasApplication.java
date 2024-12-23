3c15d2530e30f3475971eeff5a0732b3ccb6aca5
Cesar Alfredo Huanes Bautista <chuanesb@emeal.nttdata.com>
2024-12-23 13:09:20 -0500

    add contract

--- cuentasbancarias/src/main/java/com/bancos/cuentasbancarias/CuentasbancariasApplication.java
+++ cuentasbancarias/src/main/java/com/bancos/cuentasbancarias/CuentasbancariasApplication.java
@@ -2,8 +2,10 @@ package com.bancos.cuentasbancarias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.bancos.cuentasbancarias"})
 public class CuentasbancariasApplication {
 
 	public static void main(String[] args) {
