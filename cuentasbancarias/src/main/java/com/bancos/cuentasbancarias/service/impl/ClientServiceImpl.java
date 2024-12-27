6bb477836ee73d611d2e92f3e403319eb2330d08
Cesar Alfredo Huanes Bautista <chuanesb@emeal.nttdata.com>
2024-12-27 10:34:43 -0500

    add validation account

--- cuentasbancarias/src/main/java/com/bancos/cuentasbancarias/service/impl/ClientServiceImpl.java
+++ cuentasbancarias/src/main/java/com/bancos/cuentasbancarias/service/impl/ClientServiceImpl.java
@@ -1,7 +1,9 @@
 package com.bancos.cuentasbancarias.service.impl;
 
+import com.bancos.cuentasbancarias.documents.AccountType;
 import com.bancos.cuentasbancarias.documents.Client;
 import com.bancos.cuentasbancarias.documents.Account;
+import com.bancos.cuentasbancarias.documents.ClientType;
 import com.bancos.cuentasbancarias.repository.ClientDAO;
 import com.bancos.cuentasbancarias.repository.AccountDAO;
 import com.bancos.cuentasbancarias.service.ClientService;
@@ -51,23 +53,122 @@ public class ClientServiceImpl implements ClientService {
                         cliente.setCuentas(new ArrayList<>()); // Inicializar la lista si es null
                     }
 
-                    lstAccounts.forEach(cuenta -> {
-                        cuenta.setCliente_id(objectId);
-                        cuenta.setAccountType(cuenta.getAccountType());
-                        cuenta.setClient(cliente);
-                       });
-                    return accountDAO.saveAll(lstAccounts).collectList()
-                            .flatMap(savedCuentas -> {
-                                // Convertir las cuentas guardadas a ObjectId y añadirlas a la lista de cuentas del cliente
-                                List<ObjectId> savedCuentasIds = savedCuentas.stream()
-                                        .map(Account::getId)
-                                        .collect(Collectors.toList());
-                                cliente.getCuentas().addAll(savedCuentasIds);
-                                return clientDAO.save(cliente).thenReturn(savedCuentas);
-                            });
+
+                    if (ClientType.PERSONAL==cliente.getClientType()) {
+                        return validarCuentasPersonales(cliente, lstAccounts)
+                                .then(Mono.defer(() -> guardarCuentas(cliente, lstAccounts, objectId)));
+                    } else if(ClientType.EMPRESARIAL==cliente.getClientType()){
+                        return validarCuentasEmpresariales(cliente, lstAccounts)
+                                .then(Mono.defer(() -> guardarCuentas(cliente, lstAccounts, objectId)));
+                    }
+                    else {
+                        return guardarCuentas(cliente, lstAccounts, objectId);
+                    }
                 });
 
     }
 
+    private Mono<Void> validarCuentasPersonales(Client client, List<Account> lstAccounts) {
+        long ahorroCount = client.getCuentas().stream()
+                .filter(cuentaId -> accountDAO.findById(cuentaId)
+                        .map(Account::getAccountType)
+                        .block()
+                        .equals(AccountType.AHORRO))
+                .count();
+        long corrienteCount = client.getCuentas().stream()
+                .filter(cuentaId -> accountDAO.findById(cuentaId)
+                        .map(Account::getAccountType)
+                        .block()
+                        .equals(AccountType.CORRIENTE))
+                .count();
+        long plazoFijoCount = client.getCuentas().stream()
+                .filter(cuentaId -> accountDAO.findById(cuentaId)
+                        .map(Account::getAccountType)
+                        .block()
+                        .equals(AccountType.PLAZO_FIJO))
+                .count();
 
+        // Incluir las cuentas del parámetro lstAccounts en el conteo
+        for (Account cuenta : lstAccounts) {
+            switch (cuenta.getAccountType()) {
+                case AHORRO:
+                    ahorroCount++;
+                    break;
+                case CORRIENTE:
+                    corrienteCount++;
+                    break;
+                case PLAZO_FIJO:
+                    plazoFijoCount++;
+                    break;
+            }
+        }
+
+        // Validar que no se exceda el límite de una cuenta por tipo
+        if (ahorroCount > 1) {
+            return Mono.error(new IllegalArgumentException("El cliente ya tiene una cuenta de ahorro."));
+        }
+        if (corrienteCount > 1) {
+            return Mono.error(new IllegalArgumentException("El cliente ya tiene una cuenta corriente."));
+        }
+        if (plazoFijoCount > 1) {
+            return Mono.error(new IllegalArgumentException("El cliente ya tiene una cuenta de plazo fijo."));
+        }
+
+        return Mono.empty();
+    }
+    private Mono<Void> validarCuentasEmpresariales(Client client, List<Account> lstAccounts) {
+        // Contar las cuentas existentes por tipo
+        long ahorroCount = client.getCuentas().stream()
+                .filter(cuentaId -> accountDAO.findById(cuentaId)
+                        .map(Account::getAccountType)
+                        .block()
+                        .equals(AccountType.AHORRO))
+                .count();
+        long plazoFijoCount = client.getCuentas().stream()
+                .filter(cuentaId -> accountDAO.findById(cuentaId)
+                        .map(Account::getAccountType)
+                        .block()
+                        .equals(AccountType.PLAZO_FIJO))
+                .count();
+
+        // Validar que no se creen cuentas de ahorro o plazo fijo para clientes empresariales
+        for (Account cuenta : lstAccounts) {
+            if (cuenta.getAccountType() == AccountType.AHORRO) {
+                ahorroCount++;
+            }
+            if (cuenta.getAccountType() == AccountType.PLAZO_FIJO) {
+                plazoFijoCount++;
+            }
+        }
+
+        if (ahorroCount > 0) {
+            return Mono.error(new IllegalArgumentException("Un cliente empresarial no puede tener una cuenta de ahorro."));
+        }
+        if (plazoFijoCount > 0) {
+            return Mono.error(new IllegalArgumentException("Un cliente empresarial no puede tener una cuenta de plazo fijo."));
+        }
+
+        return Mono.empty();
+    }
+
+
+private Mono<List<Account>> guardarCuentas(Client client, List<Account> lstAccounts, ObjectId objectId) {
+    lstAccounts.forEach(cuenta -> {
+        cuenta.setCliente_id(objectId);
+        cuenta.setAccountType(cuenta.getAccountType());
+        cuenta.setClient(client);
+    });
+
+    return accountDAO.saveAll(lstAccounts).collectList()
+            .flatMap(savedCuentas -> {
+                // Convertir las cuentas guardadas a ObjectId y añadirlas a la lista de cuentas del cliente
+                List<ObjectId> savedCuentasIds = savedCuentas.stream()
+                        .map(Account::getId)
+                        .collect(Collectors.toList());
+                client.getCuentas().addAll(savedCuentasIds);
+                return clientDAO.save(client).thenReturn(savedCuentas);
+            });
+
+
+    }
 }
