fa9d7498a168bc67f79805423f13a90376855075
Cesar Alfredo Huanes Bautista <chuanesb@emeal.nttdata.com>
2025-01-06 16:43:26 -0500

    add configuration

--- cuentasbancarias/src/main/java/com/bancos/cuentasbancarias/CuentasbancariasApplication.java
+++ cuentasbancarias/src/main/java/com/bancos/cuentasbancarias/CuentasbancariasApplication.java
@@ -2,9 +2,11 @@ package com.bancos.cuentasbancarias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableEurekaClient
 @ComponentScan(basePackages = {"com.bancos.cuentasbancarias"})
 public class CuentasbancariasApplication {
 
